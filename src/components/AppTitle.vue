d6fe3aed01b889acae513cb90fec5904b833e7fb
DorinVieru <dorino.vieru@gmail.com>
2023-12-21 16:54:55 +0100

    [UP] Create component AppTitle & complete project

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<!-- SCRIPT JS -->
+<script>
+export default {
+
+}
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+  <div>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="text-primary">La mia prima app con Vite!</h1>               
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<!-- STYLE CSS -->
+<style lang="">
+  
+</style>
